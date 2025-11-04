9e70467a845ba633e0e367d9b9ce98a7bda216af
alard671-jpg <alard671@gmail.com>
2025-11-04 21:20:40 +0300

    Update playLive function to use embedded player

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function playLive() {
-    window.open('https://zeno.fm/radio/dream-podcast/', '_blank');
+    const player = document.getElementById('zeno-player');
+    player.style.display = 'block';
+    player.src = 'https://zeno.fm/radio/dream-podcast/embed';
+    player.scrollIntoView({behavior: 'smooth'});
 }
