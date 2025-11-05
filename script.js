0c48a1d8549c8e3dd3eed71e7a9c59ffd3068d13
alard671-jpg <alard671@gmail.com>
2025-11-05 21:16:22 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function playLive() {
-    const player = document.getElementById('zeno-player');
-    player.style.display = 'block';
-    player.src = 'https://zeno.fm/radio/dream-podcast/embed';
-    player.scrollIntoView({behavior: 'smooth'});
-}
